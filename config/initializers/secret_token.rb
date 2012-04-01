2d95e5a829b2d005b5aabfc80a4c6f5e3f7618d1
Eric Waring <ericw@the-feel.co.uk>
2012-04-01 15:30:26 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quipper::Application.config.secret_token = '27d32a1dcd0f8d4e16fb609476e41ece4319f8e9ac3952113f60d383eb27ab2181b402a101e90a23d9d9b8d103c183b3d569d36b51cefc9e4ff17321e088019d'
